e21a4e6b0245ab4ca2a03ab4745b957b53e79e6b
emilkjellin <emil.kjellin@elev.ga.ntig.se>
2024-10-21 09:35:39 +0200

    påbörjat uppgiften

---
+++ src/TvSeries.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TvSeries {
+   private String name ;
+   private ArrayList<Integer> episodes;
+   public TvSeries() {
+
+   }
+
+   public TvSeries (String inName)
+   {
+
+   }
+}

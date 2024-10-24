72dd7afd504727af86771df13a278e95f445ea51
emilkjellin <emil.kjellin@elev.ga.ntig.se>
2024-10-24 10:36:43 +0200

    jobbat

--- src/TvSeries.java
+++ src/TvSeries.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
+import java.io.Serializable;
+import java.util.Scanner;
 
-public class TvSeries {
-   private String name ;
-   private ArrayList<Integer> episodes;
+public class TvSeries implements Serializable {
+   private String name;
+   private ArrayList<Integer> episodes = new ArrayList<>();
+   private int rating;
    public TvSeries() {
 
    }
 
    public TvSeries (String inName)
    {
-
+   this.name = inName;
    }
+
+
+
 }
